c0e970bf744b02ad4c429875e3c65871d95c0702
Marcos Araújo <marcosroaraujo@gmail.com>
2019-04-05 14:51:07 -0300

    Ajuste de layout

---
+++ .history/src/app/app.component_20190405141557.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Platform } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { AngularFireAuth } from '@angular/fire/auth';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html'
+})
+export class AppComponent {
+  nome: any;
+  nomeMenu: any;
+  logado: boolean;
+  constructor(
+    private platform: Platform,
+    private splashScreen: SplashScreen,
+    private router: Router,
+    private statusBar: StatusBar,
+    public afAuth: AngularFireAuth,
+  ) {
+    this.initializeApp();
+    afAuth.authState.subscribe((user: firebase.User) => {
+      if (!user) {
+        this.nome = null;
+        return;
+      }
+      this.nome = localStorage.getItem('nome');
+      this.nomeMenu = 'Olá ' + this.nome;
+      this.logado = JSON.parse(localStorage.getItem('logado'));
+    });
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+      const usuarioLogado = localStorage.getItem('logado');
+      if (usuarioLogado) {
+        if (usuarioLogado !== 'true') {
+          this.nome = localStorage.getItem('nome');
+          this.router.navigate(['/login']);
+        }
+      } else {
+        this.router.navigate(['/login']);
+      }
+    });
+  }
+
+  sair() {
+    this.afAuth.auth.signOut();
+    this.router.navigate(['/login']);
+  }
+}
---
+++ .history/src/app/login/login.page_20190405141412.ts
@@ -0,0 +1,182 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HTTP } from '@ionic-native/http/ngx';
+import { LoadingService } from '../loading.service';
+import { AngularFireAuth } from '@angular/fire/auth';
+import * as firebase from 'firebase/app';
+import { Facebook } from '@ionic-native/facebook/ngx';
+import { Platform, ToastController } from '@ionic/angular';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+  logado: boolean;
+  respostas: any;
+  nome: any;
+  email: any;
+  displayName: any;
+  constructor(
+    public http: HTTP,
+    public afAuth: AngularFireAuth,
+    public router: Router,
+    public loading: LoadingService,
+    public fb: Facebook,
+    public platform: Platform,
+    public toastController: ToastController
+    // public facebook: Facebook
+    ) {
+      afAuth.authState.subscribe((user: firebase.User) => {
+        if (!user) {
+          this.nome = null;
+          this.email = null;
+          return;
+        }
+        this.nome = user.displayName;
+        this.email = user.email;
+      });
+    }
+
+  ngOnInit() {
+    this.checaLogado();
+  }
+  async mostraToast(message) {
+    const toast = await this.toastController.create({
+      message: message,
+      showCloseButton: true,
+      position: 'top',
+      closeButtonText: 'Fechar',
+      duration: 5000
+    });
+    toast.present();
+  }
+
+  checaLogado() {
+      if (this.displayName) {
+        this.logado = true;
+        this.router.navigate(['/tabs/tab1']);
+      } else {
+        this.logado = false;
+        this.router.navigate(['/login']);
+      }
+  }
+
+  loginComFacebook() {
+    this.loading.present();
+    if (this.platform.is('cordova')) {
+      return this.fb.login(['email', 'public_profile'])
+      .then(res => {
+        const facebookCredential = firebase.auth.FacebookAuthProvider.credential(res.authResponse.accessToken);
+        return firebase.auth().signInAndRetrieveDataWithCredential(facebookCredential)
+        .then(user => {
+          this.loading.dismiss();
+          localStorage.setItem('nome', user.displayName);
+          this.checaLogado();
+          this.router.navigate(['/tabs/tab1']);
+        });
+      });
+    } else {
+      return this.afAuth.auth
+        .signInWithPopup(new firebase.auth.FacebookAuthProvider())
+        .then(user => {
+          this.checaLogado();
+          this.loading.dismiss();
+          localStorage.setItem('nome', user.displayName));
+          this.router.navigate(['/tabs/tab1']);
+        });
+    }
+  }
+
+  sair() {
+    this.afAuth.auth.signOut();
+  }
+
+  loginComEmailSenha(form) {
+    this.loading.present();
+    return this.afAuth.auth
+      .signInWithEmailAndPassword(form.value.emailCadastro, form.value.senhaCadastro)
+      .then(res => {
+        this.checaLogado();
+        localStorage.setItem('nome', form.value.nome));
+        this.loading.dismiss();
+        localStorage.setItem('logado', 'true');
+        this.router.navigate(['/tabs/tab1']);
+      }, erro => {
+        this.loading.dismiss();
+        if (erro.code === 'auth/wrong-password') {
+          this.mostraToast('Senha errada');
+        } else if (erro.code === 'auth/user-not-found') {
+          this.mostraToast('Email não cadastrado');
+        } else {
+          this.mostraToast('Email ou Senha errada');
+        }
+      });
+  }
+
+//   doFacebookLogin() {
+//     return new Promise<FirebaseUserModel>((resolve, reject) => {
+//       if (this.platform.is('cordova')) {
+//         //["public_profile"] is the array of permissions, you can add more if you need
+//         this.fb.login(["public_profile"]).then((response) => {
+//           const facebookCredential = firebase.auth.FacebookAuthProvider.credential(response.authResponse.accessToken);
+//           firebase.auth().signInWithCredential(facebookCredential)
+//           .then((user) => {
+//             resolve()
+//           });
+//         },(err) => {
+//           reject(err);
+//         });
+//       }
+//       else {
+//         this.afAuth.auth
+//         .signInWithPopup(new firebase.auth.FacebookAuthProvider())
+//        .then((user) => {
+//           resolve()
+//        })
+//      }
+//    })
+//  }
+
+//  doLogout(){
+//   return new Promise((resolve, reject) => {
+//     if (firebase.auth().currentUser) {
+//       this.afAuth.auth.signOut();
+//       resolve();
+//     }
+//     else {
+//       reject();
+//     }
+//   });
+// }
+  // fazLogin(form: any) {
+  //   this.loading.present();
+  //   const url = 'http://www.professoramireilecosta.com.br/crud_pdo/login_json.php';
+  //   const params = form.value;
+  //   const headers = {};
+
+  //   this.http.post(url, params, headers)
+  //   .then(data => {
+  //     console.log('sucesso Login');
+  //     console.log(data.status);
+  //     console.log("TODOS",data.data); // data received by server
+  //     console.log(data.headers);
+  //     this.router.navigate(['/tabs/tab1']);
+  //     this.loading.dismiss();
+  //     localStorage.setItem('logado', 'true');
+
+  //   })
+  //   .catch(error => {
+  //     console.log('erro');
+  //     console.log(error.status);
+  //     console.log(error.error); // error message as string
+  //     console.log(error.headers);
+  //     this.router.navigate(['/login']);
+  //     this.loading.dismiss();
+  //     localStorage.setItem('logado', 'false');
+
+  //   });
+  // }
+}
